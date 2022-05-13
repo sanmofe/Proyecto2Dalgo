14c6589832ce44134d60acd74b60ecbc69155b0f
dfcruza <60239198+dfcruza@users.noreply.github.com>
2022-05-13 17:10:30 -0500

    limpio

--- ProblemaP2.py
+++ ProblemaP2.py
@@ -1,4 +1,4 @@
-"Autores: Santiago Mora y Sergio Guillen"
+"Autores: Santiago Mora, Sergio Guillen, David Cruz y Juan Castaño"
 
 import sys
 import time
@@ -27,45 +27,23 @@ def componentesDelGrafo(arr):
         return componentesDelGrafo(arrIzq) + 1
 
 
-# a= [3,2,1,6,5,4]
-# print(componentesDelGrafo(a))
-
-
-
-
-
 if __name__ == '__main__':
     
     start = time.time()
     
     lec= sys.stdin.read().split('\n')
     
-    # lec = input().split('\n')
     cont = 0 
     nCasos = int(lec[0])
-    #print("numero de casos: ", nCasos)
     for i in range(1, nCasos + 1):
         casoStr = lec[i].split(" ")
         caso = [int(i) for i in casoStr] #Convertimos los strings del arreglo en enteros
-        #print(caso)
-        #print(componentesDelGrafo(caso))
+       
         sys.stdout.write(str(componentesDelGrafo(caso)))
         sys.stdout.write("\n")
         cont+=1
-    #print("ejecuciones: ",cont)
+   
     end = time.time()
     
-    #print("TIME: ",end-start)
     final = str(end-start)
     sys.stdout.write(str("TIME: "+final))
-
-    # # Cómo vrg se leen varias líneas en Python? aaaaaaaaaaaaa
-    # Creo que yo sé B) 
-    #
-    # contents = sys.stdin.readlines()
-    # loscasos = contents.split("\n")
-    # numero = int(loscasos[0])
-
-    # for i in range(numero - 1):
-    #     arreglo = loscasos[i].split()
-    #     print(componentesDelGrafo(arreglo))
